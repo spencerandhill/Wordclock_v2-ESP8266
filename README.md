35fcb1d4d2d5d19fae83c078e7abc826b1cc3c41
spencerandhill <jan.rathmer@gmail.com>
2017-11-26 17:20:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Overview
+This Project is used to control a RGB wordclock with WS2812B LED's and an esp8266 controller.
+
+This is still **work in progress**
---
+++ main.ino
@@ -0,0 +1,28 @@
+#include <SoftwareSerial.h>
+#define FASTLED_ALLOW_INTERRUPTS 0
+#include <FastLED.h>
+#include <NeoPixelBrightnessBus.h>
+#include <Wire.h>
+#include <TimeLib.h>
+
+#define BAUD           (115200)   // Baudrate from Arduino to Host-System
+
+void setup() {
+  // Set Time to default-value
+  setRTCTime(0,0);
+
+  // General Setup
+  // Open the Serial-interface to host
+  Serial.begin(BAUD);delay(100);
+  Serial.setDebugOutput(true);
+
+  //setupClock();     //Setup RTC-Modul Stuff
+  setupWebserver();   //Setup Webserver-Modul Stuff
+  setupLED();         //Setup LED-Modul Stuff
+  delay(1000);
+}
+
+void loop() {
+  loopWebserver();    //Check Webserver activities
+  loopLED();          //Get Time from RTC Module and SET the related LED's
+}
